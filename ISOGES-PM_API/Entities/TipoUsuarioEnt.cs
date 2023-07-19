44c17f8bf8a678843df81b21bd71557c5e0a6b1f
Valeria Ali <vsalif123@gmail.com>
2023-07-19 14:43:32 -0600

    agregar archivos

---
+++ ISOGES-PM_API/Entities/TipoUsuarioEnt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISOGES_PM_API.Entities
+{
+    public class TipoUsuarioEnt
+    {
+        public int IdTipoUsuario { get; set; }
+        public string NombreTipo { get; set; }
+    }
+}
