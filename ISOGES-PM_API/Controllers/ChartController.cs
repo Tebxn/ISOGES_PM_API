617c7e0cd3380f3ae3b0070bc778b1b49e6cdf17
Esteban Sanchez <easanrui@gmail.com>
2023-08-05 14:50:33 -0600

    Controladores y entidades para los graficos

---
+++ ISOGES-PM_API/Controllers/ChartController.cs
@@ -0,0 +1,55 @@
+﻿using ISOGES_PM_API.Entities;
+using ISOGES_PM_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ISOGES_PM_API.Controllers
+{
+    public class ChartController : ApiController
+    {
+        [HttpGet]
+        [Route("api/CargarChartAnualIngreso")]
+        public List<ChartAnualIngresoEnt> CargarChartAnualIngreso()
+        {
+            using (var bd = new ISOGES_PMEntities())
+            {
+                var datos = (from x in bd.Cobro
+                             select x).ToList();
+
+                if (datos.Count > 0)
+                {
+                    var resp = new List<ChartAnualIngresoEnt>();
+
+                    foreach (var item in datos)
+                    {
+                        DateTime dateTime = item.Fecha;
+                        DateTime anioSolo = dateTime.Date;
+                        DateTime diaMesSolo = dateTime.Date;
+                        DateTime mesSolo = dateTime.Date;
+                        string anioEnviar = anioSolo.ToString("yyyy");
+                        string diaMesEnviar = diaMesSolo.ToString("dd/MM");
+                        string mesSoloEnviar = mesSolo.ToString("MM");
+
+                        resp.Add(new ChartAnualIngresoEnt
+                        {
+                            Anio = anioEnviar,
+                            Mes = mesSoloEnviar,
+                            DiaMes = diaMesEnviar,
+                            IngresosTotales = item.Monto,
+                        });
+                    }
+                    return resp;
+                }
+                else
+                {
+                    return new List<ChartAnualIngresoEnt>();
+                }
+            }
+        }
+    }
+}
+
---
+++ ISOGES-PM_API/Entities/ChartEnt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISOGES_PM_API.Entities
+{
+    public class ChartEnt
+    {
+
+    }
+    public class ChartAnualIngresoEnt
+    {
+        public string Anio { get; set; }
+        public string Mes { get; set; }
+        public string DiaMes { get; set; }
+        public double IngresosTotales { get; set; }
+    }
+}
