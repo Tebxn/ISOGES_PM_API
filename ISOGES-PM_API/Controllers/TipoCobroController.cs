7ac075421294a4375adf102314babe37f14ac1c3
Valeria Ali <vsalif123@gmail.com>
2023-08-20 22:38:41 -0600

    Se agrega entidad tipoCobro, controlador TipoCobro

---
+++ ISOGES-PM_API/Controllers/TipoCobroController.cs
@@ -0,0 +1,48 @@
+﻿using ISOGES_PM_API.Entities;
+using ISOGES_PM_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ISOGES_PM_API.Controllers
+{
+    [Authorize]
+    public class TipoCobroController : ApiController
+    {
+        [HttpGet]
+        [Route("api/ConsultarTipoCobros")]
+        public List<TipoCobroEnt> ConsultarTipoCobros()
+        {
+            using (var bd = new ISOGES_PMEntities())
+            {
+                var datos = (from x in bd.TipoCobro
+                             select x).ToList();
+
+
+                if (datos.Count > 0)
+                {
+                    var resp = new List<TipoCobroEnt>();
+                    foreach (var item in datos)
+                    {
+                        resp.Add(new TipoCobroEnt
+                        {
+                            IdTipoCobro = item.IdTipoCobro,
+                            NombreTipoCobro = item.TipoCobro1
+                            
+                        });
+                    }
+                    return resp;
+                }
+                else
+                {
+                    return new List<TipoCobroEnt>();
+                }
+            }
+        }
+
+    }
+}
+
---
+++ ISOGES-PM_API/Entities/TipoCobroEnt.cs
@@ -0,0 +1,21 @@
+﻿using ISOGES_PM_API.Models.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISOGES_PM_API.Entities
+{
+    public class TipoCobroResponse : Response
+    {
+        public CobroEnt ObjectSingle { get; set; } = new CobroEnt();
+        public List<CobroEnt> ObjectList { get; set; } = new List<CobroEnt>();
+    }
+
+    public class TipoCobroEnt
+    {
+        public int IdTipoCobro { get; set; }
+        public string NombreTipoCobro { get; set; }
+      
+    }
+}
