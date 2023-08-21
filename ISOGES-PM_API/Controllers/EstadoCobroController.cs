ed207344f707ca54cb300506949b48200753aa06
Valeria Ali <vsalif123@gmail.com>
2023-08-21 00:02:11 -0600

    Se agrega estadoCobro entidad y controlador.

---
+++ ISOGES-PM_API/Controllers/EstadoCobroController.cs
@@ -0,0 +1,48 @@
+﻿using ISOGES_PM_API.Entities;
+using ISOGES_PM_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ISOGES_PM_API.Controllers
+{
+    [Authorize]
+    public class EstadoCobroController : ApiController
+    {
+        [HttpGet]
+        [Route("api/ConsultarEstadoCobros")]
+        public List<EstadoCobroEnt> ConsultarEstadoCobros()
+        {
+            using (var bd = new ISOGES_PMEntities())
+            {
+                var datos = (from x in bd.Estado_Cobro
+                             select x).ToList();
+
+
+                if (datos.Count > 0)
+                {
+                    var resp = new List<EstadoCobroEnt>();
+                    foreach (var item in datos)
+                    {
+                        resp.Add(new EstadoCobroEnt
+                        {
+                            IdEstadoCobro = item.IdEstadoCobro,
+                            NombreEstado = item.NombreEstado
+                            
+                        });
+                    }
+                    return resp;
+                }
+                else
+                {
+                    return new List<EstadoCobroEnt>();
+                }
+            }
+        }
+
+    }
+}
+
---
+++ ISOGES-PM_API/Entities/EstadoCobroEnt.cs
@@ -0,0 +1,22 @@
+﻿using ISOGES_PM_API.Models.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISOGES_PM_API.Entities
+{
+    public class EstadoCobroResponse : Response
+    {
+        public EstadoCobroEnt ObjectSingle { get; set; } = new EstadoCobroEnt();
+        public List<EstadoCobroEnt> ObjectList { get; set; } = new List<EstadoCobroEnt>();
+    }
+
+    public class EstadoCobroEnt
+    {
+        public long IdEstadoCobro { get; set; }
+        public string NombreEstado { get; set; } 
+    }
+
+    
+}
